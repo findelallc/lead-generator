42876ff3323e0915e0d4ba887f71f47faf9ffc8e
Tamaghna Banerjee <tamaghna.ba@imerit.net>
2023-08-01 20:43:45 +0530

    Folder structure changed

---
+++ frontend/public/js/script.js
@@ -0,0 +1,38 @@
+
+const apiUrl= "http://localhost:5002/lead";
+$(document).ready(function() {
+    $("#lead_generator").submit(function (e) { 
+        e.preventDefault();
+        inputs = {};
+        input_serialized =  $(this).serializeArray();
+        input_serialized.forEach(field => {
+          inputs[field.name] = field.value;
+        })
+        
+        $.ajax({
+            url: apiUrl + "/new",
+            type: 'POST',
+            data: inputs,
+            success: function(data) {
+                console.log(data);
+                if(data.status === 201) {
+                    $.toast({
+                        heading: 'Success',
+                        text: 'Your form submited successfully',
+                        position: 'top-center',
+                        stack: false,
+                        icon: 'success'
+                    })
+                    $('form#lead_generator').trigger("reset");
+                }
+            },
+            fail: function(xhr, textStatus, errorThrown){
+                $.toast({
+                    heading: 'Error',
+                    text: 'An unexpected error occured while trying to submit data.',
+                    icon: 'error'
+                })
+             }
+        });  
+    });
+});
